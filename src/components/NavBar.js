356bec06258873a6e06b06c791171a0534dba58e
rovenelabanga <rovenelabanga2001@gmail.com>
2024-10-30 17:47:03 +0300

    uses lists and keys to render array elements

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const displayLinks = links.map((link, index) => {
+    return (
+      <a href={`#${link}`} key={index}>
+        {link}
+      </a>
+    );
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{displayLinks}</nav>;
 }
 
 export default NavBar;
